320bcb22c9223285e4fb6cb10d0b07e0b4ef82e0
jastiso <jeni.stiso@gmail.com>
2020-01-19 13:49:35 -0500

    added each title to google scholar display

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,15 @@
 // get all papers on the page
-$(".gs_rt").mouseenter(function(){
-    var text = $(this).text()
-    alert(text);
-  });
+$(document).ready(function() {
+  $(".gs_rt").children().each(function(){
+    var title = $(this).text()
+    if (!($(this).hasClass('gs_ctc') || $(this).hasClass('gs_ctu'))){
+      $( "<p>" + title + "</p>" ).insertAfter($(this).parent())
+    }
+  }) 
+});
+
+// $(".gs_rt").mouseenter(function(){
+//     var text = $(this).text()
+//     alert(text);
+//   });
   
