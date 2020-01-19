279e89fc3ca98d5a380a8eba3c0261fd31865c3b
jastiso <jeni.stiso@gmail.com>
2020-01-19 14:40:37 -0500

    added four images. in the future these will be FA gender LA gander FA race LA race

--- extension/content.js
+++ extension/content.js
@@ -6,8 +6,10 @@ $(document).ready(function() {
     // check that isnt a [BOOK] or [CITATION] tag
     if (!($(this).hasClass('gs_ctc') || $(this).hasClass('gs_ctu'))){
       $( "<p>" + title + "</p>" ).insertAfter($(this).parent())
-      $( '<img id="pic" src="https://emojis.slackmojis.com/emojis/images/1471119457/987/parrot.gif?1471119457" />' ).insertAfter($(this).parent())
-      $('.pic').append('<img id="pic" src="https://emojis.slackmojis.com/emojis/images/1471119457/987/parrot.gif?1471119457" />')
+      $( '<img src="https://emojis.slackmojis.com/emojis/images/1578178080/7438/verified.png?1578178080" />' ).insertAfter($(this).parent())
+      $( '<img src="https://emojis.slackmojis.com/emojis/images/1578178080/7438/verified.png?1578178080" />' ).insertAfter($(this).parent())
+      $( '<img src="https://emojis.slackmojis.com/emojis/images/1578178080/7438/verified.png?1578178080" />' ).insertAfter($(this).parent())
+      $( '<img src="https://emojis.slackmojis.com/emojis/images/1578178080/7438/verified.png?1578178080" />' ).insertAfter($(this).parent())
     }
   }) 
 });
