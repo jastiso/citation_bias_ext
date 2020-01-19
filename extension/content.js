4812fa7711ada8f52ba157071bc10f8b5f11fe12
jastiso <jeni.stiso@gmail.com>
2020-01-19 14:31:30 -0500

    added image to scholar page

--- extension/content.js
+++ extension/content.js
@@ -2,14 +2,13 @@
 $(document).ready(function() {
   $(".gs_rt").children().each(function(){
     var title = $(this).text()
+
+    // check that isnt a [BOOK] or [CITATION] tag
     if (!($(this).hasClass('gs_ctc') || $(this).hasClass('gs_ctu'))){
       $( "<p>" + title + "</p>" ).insertAfter($(this).parent())
+      $( '<img id="pic" src="https://emojis.slackmojis.com/emojis/images/1471119457/987/parrot.gif?1471119457" />' ).insertAfter($(this).parent())
+      $('.pic').append('<img id="pic" src="https://emojis.slackmojis.com/emojis/images/1471119457/987/parrot.gif?1471119457" />')
     }
   }) 
 });
-
-// $(".gs_rt").mouseenter(function(){
-//     var text = $(this).text()
-//     alert(text);
-//   });
   
