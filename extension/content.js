e4b027c3cc149c24bdb6454850a2ec01d74496ac
jastiso <jeni.stiso@gmail.com>
2020-01-22 14:59:17 -0500

    got annimated circle working, but does not stop at given percent

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,10 @@
 // get all papers on the page
 $(document).ready(function() {
+  
+ // $(".gs_ab_md").append( '<div class="wrapper" data-anim="base wrapper"><div class="circle" data-anim="base left"></div><div class="circle" data-anim="base right"></div></div>' )
+
   $(".gs_rt").children().each(function(){
+
     // get url-encoded title
     var title = "title:'"+$(this).text()+"'"
     uri = encodeURI(title)
@@ -8,7 +12,15 @@ $(document).ready(function() {
 
     // check that isnt a [BOOK] or [CITATION] tag
     if (!($(this).hasClass('gs_ctc') || $(this).hasClass('gs_ctu'))){
-      fetch(api_req)
+      $( '<div class="wrapper" data-anim="base wrapper"><div class="circle" data-anim="base left"></div></div>' )
+      .insertAfter($(this).parent())
+      $( '<div class="wrapper" data-anim="base wrapper"><div class="circle" data-anim="base left"></div></div>' )
+      .insertAfter($(this).parent())
+      $( '<div class="wrapper" data-anim="base wrapper"><div class="circle" data-anim="base left"></div></div>' )
+      .insertAfter($(this).parent())
+      $( '<div class="wrapper" data-anim="base wrapper"><div class="circle" data-anim="base left"></div></div>' )
+      .insertAfter($(this).parent())
+     /* fetch(api_req)
       .then( (data) => data.json())
       .then( (info) => get_names(info))
       .catch(function(error) {
@@ -22,7 +34,7 @@ $(document).ready(function() {
           // drop F1000 reviews, and check if correct match wasnt first result
           var title = info.message.items[0].title[0]
           var cnt = 0
-          while (title.includes('Faculty of 1000') | title.toLowerCase() != $(this).text().toLowerCase()){
+          while (title.includes('Faculty of 1000') | !(title.toLowerCase().includes($(this).text().toLowerCase().split('.').join("")))){
             cnt = cnt + 1
             title = info.message.items[cnt].title[0]
           }
@@ -51,8 +63,17 @@ $(document).ready(function() {
           LA_family = LA_family.replace('.', ' ').replace(/"/g, "")
 
           // query genderize.io
-          const gen_url = "https://api.genderize.io?name[]="
-          fetch(gen_url + FA_given + "&name[]=" + LA_given)
+          const gen_url = "https://api.genderize.io?name"
+          if (FA_given != "" & LA_given != "")
+            fetch(gen_url + "[]=" + FA_given + "&name[]=" + LA_given)
+            .then( (data) => data.json())
+            .then( (info) => get_gender(info, FA_given, FA_family, LA_given, LA_family))
+        } else if (FA_given == "" & LA_given != ""){
+          fetch(gen_url + "=" + LA_given)
+          .then( (data) => data.json())
+          .then( (info) => get_gender(info, FA_given, FA_family, LA_given, LA_family))
+        } else if (FA_given != "" & LA_given == ""){
+          fetch(gen_url + "=" + FA_given)
           .then( (data) => data.json())
           .then( (info) => get_gender(info, FA_given, FA_family, LA_given, LA_family))
         }
@@ -60,16 +81,28 @@ $(document).ready(function() {
 
       // get the gender data from genderize.io
       const get_gender = (info, FA_given, FA_family, LA_given, LA_family) => {
-        FA_gen = JSON.stringify(info[0].gender)
-        FA_prob = JSON.stringify(info[0].probability)
-        LA_gen = JSON.stringify(info[1].gender)
-        LA_prob = JSON.stringify(info[1].probability)
+        if (FA_given != "" & LA_given != ""){
+          FA_gen = JSON.stringify(info[0].gender)
+          FA_prob = JSON.stringify(info[0].probability)
+          LA_gen = JSON.stringify(info[1].gender)
+          LA_prob = JSON.stringify(info[1].probability)
+        } else if (FA_given == "" & LA_given != ""){
+          FA_gen = ""
+          FA_prob = ""
+          LA_gen = JSON.stringify(info[0].gender)
+          LA_prob = JSON.stringify(info[0].probability)
+        } else if (FA_given == "" & LA_given != ""){
+          LA_gen = ""
+          LA_prob = ""
+          FA_gen = JSON.stringify(info[0].gender)
+          FA_prob = JSON.stringify(info[0].probability)
+        }
 
          // display
         $( "<p>First author:" + FA_given + " " + FA_family + " gender: " + FA_gen + " " + FA_prob
         + "; Last author: " + LA_given + " " + LA_family + " gender: " + LA_gen + " " + LA_prob +
          "</p>" ).insertAfter($(this).parent())
-        $( '<img src="https://emojis.slackmojis.com/emojis/images/1578178080/7438/verified.png?1578178080" />' )
+        $( '<div class="wrapper" data-anim="base wrapper"><div class="circle" data-anim="base left"></div><div class="circle" data-anim="base right"></div></div>' )
         .insertAfter($(this).parent())
         $( '<img src="https://emojis.slackmojis.com/emojis/images/1578178080/7438/verified.png?1578178080" />' )
         .insertAfter($(this).parent())
@@ -77,8 +110,8 @@ $(document).ready(function() {
         .insertAfter($(this).parent())
         $( '<img src="https://emojis.slackmojis.com/emojis/images/1578178080/7438/verified.png?1578178080" />' )
         .insertAfter($(this).parent())  
-      }
-    }
+      } */
+    } 
   }) 
 })
   
